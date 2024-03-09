67f2fc8e3546926117f480351693acb2530aa38e
AnnabelRe <annabelresch@freenet.de>
2024-03-09 10:41:54 +0100

    adds stat testing

--- .ipynb_checkpoints/calculate_AR-checkpoint.py
+++ .ipynb_checkpoints/calculate_AR-checkpoint.py
@@ -5,7 +5,7 @@ from sklearn.linear_model import LinearRegression
 def calc_windows(event_day,dr,L1,L2):
     
     if event_day not in dr:
-        #print("event day not in dr: "+str(event_day))
+        #print("event day not in daily return: "+str(event_day)+" " +str(dr.index[-1]))
         return pd.DataFrame(np.zeros((1, 200))).astype("float32"),pd.DataFrame(np.zeros((1, 41))).astype("float32")
         
     #same index for market and company
@@ -14,22 +14,26 @@ def calc_windows(event_day,dr,L1,L2):
     T1_index = (event_index-1)-L2
     T2_index = (event_index)+L2
     T0_index = T1_index-L1
-    T3_index = T2_index+L1
     #print(event_day,T0_index, T1_index,T2_index,T3_index) 
 
-    
     # Estimation window
     estimation_window = dr.iloc[:,T0_index:T1_index]
     
-    if (estimation_window.transpose().isna().sum().all()>100):
-        return pd.DataFrame(np.zeros((1, 200))).astype("float32"),pd.DataFrame(np.zeros((1, 41))).astype("float32")
-    estimation_window.transpose().fillna(0.0,inplace=True)
+    count = estimation_window.transpose().value_counts()
+    if 0.000000 in count: 
+        if count[0.000000]>100:
+            #print("More than 100 entries with 0.0 in estimation window: "+str(event_day)+" " +str(dr.index[-1]))
+            return pd.DataFrame(np.zeros((1, 200))).astype("float32"),pd.DataFrame(np.zeros((1, 41))).astype("float32")
+        #estimation_window.transpose().fillna(0.0,inplace=True)
     
     # Event-Window
     event_window = dr.iloc[:,T1_index:T2_index]
-    if (event_window.transpose().isna().sum().all()>19):
-        return pd.DataFrame(np.zeros((1, 200))).astype("float32"),pd.DataFrame(np.zeros((1, 41))).astype("float32")
-    event_window.transpose().fillna(0.0,inplace=True)
+    count = event_window.transpose().value_counts()
+    if 0.000000 in count:
+        if count[0.000000]>19:
+            #print("More than 19 entries with 0.0 in event window: "+str(event_day)+" " +str(dr.index[-1]))
+            return pd.DataFrame(np.zeros((1, 200))).astype("float32"),pd.DataFrame(np.zeros((1, 41))).astype("float32")
+    #event_window.transpose().fillna(0.0,inplace=True)
     #print(event_window.shape)
     
     return estimation_window,event_window
@@ -59,12 +63,19 @@ def run_calculation_AR(date,cr,mr, L1 = 200, L2 = 20):
     est_window_market,event_window_market = calc_windows(date,mr,L1,L2)
     est_window_comp,event_window_comp = calc_windows(date,cr,L1,L2)
     
-    if (est_window_market.iloc[:,-1:].isnull().values.any() or est_window_comp.iloc[:,-1:].isnull().values.any()):
-        return pd.DataFrame(np.zeros((1, 41))).astype("float32")
-    
-    a,b = calc_linreg(est_window_market.values[0,:],est_window_comp.values[0,:])
+    count_1 = est_window_market.transpose().value_counts()
+    count_2 = event_window_market.transpose().value_counts()
+    count_3 = est_window_comp.transpose().value_counts()
+    count_4 = event_window_comp.transpose().value_counts()
+        
+    if ((len(count_1) > 1)&(len(count_2) > 1)&(len(count_3) > 1)&(len(count_4) > 1)):
+        
+        a,b = calc_linreg(est_window_market.values[0,:],est_window_comp.values[0,:])
 
-    AR = event_window_comp.values + a - (b*event_window_market.values)
+        AR = event_window_comp.values + a - (b*event_window_market.values)
+        eps = est_window_comp.values - (est_window_market.values*b)
+        return AR, est_window_market,event_window_market,eps
 
-    return AR
+    else:
+        return pd.DataFrame(np.zeros((1, 41))).astype("float32"),est_window_market,event_window_market,pd.DataFrame(np.zeros((1, 200))).astype("float32")
     
--- import_data.py
+++ import_data.py
@@ -9,7 +9,7 @@ from tqdm.contrib.concurrent import thread_map
 
 url = "http://openinsider.com/screener?s={ticker}&o=&pl=&ph=&ll=&lh=&fd=0&fdr=&td=0&tdr=&fdlyl=&fdlyh=&daysago=&xp=1&xs=1&vl=&vh=&ocl=&och=&sic1=-1&sicl=100&sich=9999&grp=0&nfl=&nfh=&nil=&nih=&nol=&noh=&v2l=&v2h=&oc2l=&oc2h=&sortcol=0&cnt=1000&page={page_no}"
 
-nasdaq = pd.read_excel("/Users/admin/Documents/IP/data/ISIN_merge.xlsx") 
+nasdaq = pd.read_excel("/Users/admin/Documents/IP/data/Input/ISIN_merge.xlsx") 
 TICKERS = nasdaq['TICKER SYMBOL'].tolist()
 
 import_length = 1000
